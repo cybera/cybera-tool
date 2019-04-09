1dc7beda5437be11e65cf7c725594bb010fb4a86
Micheal Jones <micheal@mcjones.ca>
2019-04-09 15:39:32 -0600

    Tweaks on HTML filtering to deal with new interface that isn't run on Drupal

--- timesheet/log.go
+++ timesheet/log.go
@@ -13,14 +13,14 @@ import (
 )
 
 const (
-	logUrl = baseUrl + "/userts/add"
+	logUrl = baseUrl + "/index.php?newent=y"
 )
 
 var Today = time.Now().Format(timeLayout)
 
 func parseAccounts(doc *goquery.Document) map[string]string {
 	accounts := make(map[string]string)
-	doc.Find("#edit-selaccount").ChildrenFiltered("option").Each(
+	doc.Find("select").ChildrenFiltered("option").Each(
 		func(i int, opt *goquery.Selection) {
 			accounts[opt.Text()], _ = opt.Attr("value")
 		})
@@ -34,17 +34,22 @@ func Log(dates []string, time time.Duration, account, descr string, noop bool) {
 	res, _ := c.Get(logUrl)
 	doc := parseResponse(res)
 
-	formBuildId, exists := doc.Find("#timepunchextuni-form input[name='form_build_id']").Attr("value")
-	if !exists {
-		log.Fatal("Can't find timesheet form build id")
-	}
-	formToken, exists := doc.Find("#edit-timepunchextuni-form-form-token").Attr("value")
-	if !exists {
-		log.Fatal("Can't find form token")
+	//Deal with csrf
+	csrf_value := ""
+
+	// CSRF - is stored in the cookie
+	if res.StatusCode == 200 {
+		cookies := c.Jar.Cookies(baseUrlCanonical)
+		for _, v := range cookies {
+			if v.Name == "ctsrtx" {
+				csrf_value = v.Value
+			}
+		}
 	}
 
 	// The form expects an account ID, not name
 	accounts := parseAccounts(doc)
+	log.Println(accounts)
 	accId := accounts[account]
 	if accId == "" {
 		log.Fatal(errors.New("Account not found."))
@@ -52,20 +57,16 @@ func Log(dates []string, time time.Duration, account, descr string, noop bool) {
 
 	// Fill the form
 	tsAddRequest := url.Values{
-		"fromdate[date]": {dates[0]}, // TODO: UNSAFE
-		"todate[date]":   {dates[0]}, // TODO: Implement ranges
-		"hours":          {strconv.FormatFloat(time.Hours(), 'f', 2, 64)},
-		"op":             {"Submit"},
-		"selaccount":     {accId},
-		"notes":          {descr},
-		"userext":        {},
-		"destination":    {"userts/add"},
-		"autorefresh":    {"0"},
-		"form_build_id":  {formBuildId},
-		"form_token":     {formToken},
-		"form_id":        {"timepunchextuni_form"},
+		"entry_date":        {dates[0]}, // TODO: UNSAFE
+		"entry_todate":      {dates[0]}, // TODO: Implement ranges
+		"entry_hours":       {strconv.FormatFloat(time.Hours(), 'f', 2, 64)},
+		"go":                {"Create Timesheet entry"},
+		"account_id":        {accId},
+		"entry_description": {descr},
+		"add":               {"y"},
+		"ctsrtx":            {csrf_value},
 	}
-	fmt.Printf("Logging %s hours for %s\n", tsAddRequest["hours"][0], tsAddRequest["fromdate[date]"][0])
+	fmt.Printf("Logging %s hours for %s\n", tsAddRequest["entry_hours"][0], tsAddRequest["entry_date"][0])
 
 	if noop {
 		fmt.Printf("Would have POST'd: %+v\n", tsAddRequest)
@@ -74,7 +75,8 @@ func Log(dates []string, time time.Duration, account, descr string, noop bool) {
 		res, _ = c.PostForm(logUrl, tsAddRequest)
 
 		doc = parseResponse(res)
-		confirmation := doc.Find(".messages").Text()
+		confirmation := doc.Find("html").Text()
+		log.Println(confirmation)
 		if len(strings.TrimSpace(confirmation)) < 1 {
 			log.Fatal(errors.New("No confirmation recieved"))
 		}
